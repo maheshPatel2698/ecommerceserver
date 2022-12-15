cf7b486dc24d50ae795f0bfbb0f4aa59ce92746f
Mahesh Patel <mp8148515@gmail.com>
2022-12-15 20:24:17 +0530

    product route setup

--- Models/Product.js
+++ Models/Product.js
@@ -2,6 +2,51 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
 
+    product_name: {
+        type: String,
+        required: true
+    },
+    product_price: {
+        type: String,
+        required: true
+    },
+    product_description: {
+        type: String,
+        required: true
+    },
+    product_photos: [
+        {
+            secure_id: {
+                type: String,
+                required: true
+            },
+            secure_url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    product_reviews: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            product_review: {
+                type: String,
+            }
+        }
+    ],
+    product_rating: {
+        type: Number,
+        default: 0
+    },
+    avaliable_quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
 })
 
 
--- Routes/product.js
+++ Routes/product.js
@@ -1,11 +1,98 @@
 const express = require('express')
 const productRouter = express.Router()
 const Product = require('../Models/Product')
-
+const isAdmin = require('../Middleware/isAdmin')
 
 
 productRouter.get('/', (req, res) => {
     res.send('productRouter')
+})
+
+productRouter.get('/allproduct', async (req, res) => {
+
+})
+
+productRouter.get('/getoneproduct', async (req, res) => {
+
+})
+
+
+productRouter.post('/addproduct', isAdmin, async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+            error: error.message
+        })
+    }
+})
+
+
+productRouter.put('/updateproduct', isAdmin, async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+            error: error.message
+        })
+    }
+})
+
+
+productRouter.delete('/deleteproduct', isAdmin, async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+            error: error.message
+        })
+    }
+})
+
+
+productRouter.get('/getoneproduct', isAdmin, async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+            error: error.message
+        })
+    }
+})
+
+
+productRouter.put('/addquantity', isAdmin, async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+            error: error.message
+        })
+    }
+})
+
+
+productRouter.put('/changeprice', isAdmin, (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+            error: error.message
+        })
+    }
+
 })
 
 
